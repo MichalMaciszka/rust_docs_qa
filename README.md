753ea448e7f19d1d95f0eac18a483932c369f605
Michał Maciszka <t-ja15@wp.pl>
2023-10-04 18:22:06 +0200

    first version

---
+++ README.md
@@ -0,0 +1,6 @@
+## projekt badawczy
+- Generalnie to jest to mniej więcej to co było w pilot study ale w nowszej bibliotece
+- Działa no może trochę lepiej chociaż cudów nie ma, trzeba trochę popróbować z modelami, parametrami itd. Jakoś więcej pomysłów mi teraz nie przychodzi do głowy, może to adnotowanie jak przy poprzedniej wersji ale tam to tylko przeszkadzało w sumie
+- Tak sobie myślę, że możemy zrobić ten zbiór testowy o którym mówiliśmy na konsultacjach kiedyś, posprawdzać na nim różne metryki, sprawdzić różne opcje z tej biblioteki, opisać to jakoś i tyle, nawet jeśli wyniki będą jakieś giga słabe xD
+- Dokumentacja jest z repo rusta, myślę że wystarczy o ile pytania będziemy wybierali z tego co jest, też zresztą nie chodzi o to żeby tego było mega dużo
+- Dokumentacja tej biblioteki: https://docs.haystack.deepset.ai/docs
---
+++ qa.py
@@ -0,0 +1,106 @@
+import logging
+import time
+
+logging.basicConfig(format="%(levelname)s - %(name)s -  %(message)s", level=logging.DEBUG)
+logging.getLogger("haystack").setLevel(logging.INFO)
+logger = logging.getLogger("haystack")
+
+from haystack.document_stores import InMemoryDocumentStore
+from haystack.nodes import MarkdownConverter
+from haystack.pipelines import Pipeline, ExtractiveQAPipeline
+from pathlib import Path
+from haystack.nodes import FARMReader, TransformersReader, EmbeddingRetriever
+from haystack.nodes.file_classifier import FileTypeClassifier
+from haystack.nodes.preprocessor import PreProcessor
+from haystack.utils import print_answers
+from haystack.nodes.prompt import PromptModel, PromptNode
+
+
+def basic_qa():
+    paths = [p for p in Path("rust_book").glob("**/*")]
+
+    document_store = InMemoryDocumentStore(use_bm25=True, use_gpu=True)
+    indexing_pipeline = Pipeline()
+
+    classifier = FileTypeClassifier(supported_types=["md"])
+    indexing_pipeline.add_node(classifier, name="Classifier", inputs=["File"])
+
+    converter = MarkdownConverter()
+    indexing_pipeline.add_node(converter, name="Converter", inputs=["Classifier.output_1"])
+
+    preprocessor = PreProcessor(
+        clean_whitespace=False,
+        clean_empty_lines=False,
+        split_length=30,
+        split_overlap=10,
+        split_respect_sentence_boundary=True,
+    )
+    indexing_pipeline.add_node(preprocessor, name="Preprocessor", inputs=["Converter"])
+
+    indexing_pipeline.add_node(document_store, name="Document store", inputs=["Preprocessor"])
+
+    indexing_pipeline.run(file_paths=paths)
+
+    # retriever = BM25Retriever(document_store=document_store)
+    retriever = EmbeddingRetriever(document_store=document_store, embedding_model="sentence-transformers/all-mpnet-base-v2", use_gpu=True)
+    reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=True)
+    prompt_model = PromptModel()
+    prompt_node = PromptNode(prompt_model, default_prompt_template="deepset/question-answering-per-document")
+
+    document_store.update_embeddings(retriever)
+
+    # Query Pipeline
+    pipeline = Pipeline()
+    pipeline.add_node(component=retriever, name="Retriever", inputs=["Query"])
+    pipeline.add_node(component=reader, name="Reader", inputs=["Retriever"])
+    pipeline.add_node(component=prompt_node, name="Prompt node", inputs=["Reader"])
+
+    while True:
+        question = input("Question: ")
+        prediction = pipeline.run(
+            query=question, params={"Retriever": {"top_k": 100}, "Reader": {"top_k": 7}}
+        )
+        print_answers(prediction, details="minimum")
+
+def different_qa():
+    reader = TransformersReader("ahotrod/albert_xxlargev1_squad2_512", use_gpu=True)
+    paths = [p for p in Path("rust_book").glob("**/*")]
+    logger.warning(paths)
+    time.sleep(10)
+
+    document_store = InMemoryDocumentStore(use_bm25=True, use_gpu=True)
+    indexing_pipeline = Pipeline()
+
+    classifier = FileTypeClassifier(supported_types=["md"])
+    indexing_pipeline.add_node(classifier, name="Classifier", inputs=["File"])
+    converter = MarkdownConverter()
+    indexing_pipeline.add_node(converter, name="Converter", inputs=["Classifier.output_1"])
+
+    preprocessor = PreProcessor(
+        clean_whitespace=True,
+        clean_empty_lines=True,
+        split_length=100,
+        split_overlap=50,
+        split_respect_sentence_boundary=True,
+    )
+    indexing_pipeline.add_node(preprocessor, name="Preprocessor", inputs=["Converter"])
+
+    indexing_pipeline.add_node(document_store, name="Document store", inputs=["Preprocessor"])
+
+    indexing_pipeline.run(file_paths=paths)
+    retriever = EmbeddingRetriever(document_store=document_store, embedding_model="sentence-transformers/all-mpnet-base-v2")
+    pipeline = ExtractiveQAPipeline(reader, retriever)
+    document_store.update_embeddings(retriever=retriever)
+
+    while True:
+        question = input("Question: ")
+        prediction = pipeline.run(
+            query=question, params={"Retriever": {"top_k": 10}, "Reader": {"top_k": 5}}
+        )
+        print_answers(prediction, details="all")
+        
+
+if __name__ == "__main__":
+    # launch_es()
+    basic_qa()
+    # different_qa()
