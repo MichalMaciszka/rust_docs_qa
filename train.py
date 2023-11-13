930683b054f881061185f964efb34103e4d543c5
Michał Maciszka <t-ja15@wp.pl>
2023-11-13 12:14:04 +0100

    dataset split

--- train.py
+++ train.py
@@ -8,5 +8,14 @@ logger = logging.getLogger("haystack")
 from haystack.nodes import FARMReader
 
 if __name__ == "__main__":
-    reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=True)
-    reader.train(data_dir="annotation", train_filename="final_dataset.json", use_gpu=True, n_epochs=3, save_dir=".")
+    reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=False)
+    reader.train(data_dir="annotation",
+                 train_filename="train_dataset.json",
+                 dev_filename="validation_dataset.json",
+                 test_filename="test_dataset.json",
+                 use_gpu=False,
+                 n_epochs=10,
+                 save_dir="models",
+                 checkpoint_every=300,
+                 checkpoints_to_keep=10,
+                 batch_size=8)
